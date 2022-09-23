6695cb6be4dbe40835665744d6ef28212d076ccd
Junkyu Park <flowershy@unionpos.co.kr>
2022-09-23 18:56:29 +0900

    LoveTest - 1

--- app/src/main/java/com/example/lovetest/MainActivity.kt
+++ app/src/main/java/com/example/lovetest/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.lovetest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+    lateinit var navController : NavController  // 나중에 정의한다는 예약어어
+
+   override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+       navController = nav_host_fragment.findNavController()
     }
 }
